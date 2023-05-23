7b70c3e3b922c3136c95efe2a29d98de8dd1963f
MutasemShboul <mutasemshboul14@gmail.com>
2023-05-23 22:44:21 +0300

    Merge branch 'master' of https://github.com/Mutasemshboul/Git-Assignment
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    //My name is mutasem
-        // Hello from this acount
-        // Nasser
-    //My name is mutasem Hi from another account
-       //Helllo
-        //Hiii
+
     }
 }
