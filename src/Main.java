163011caf15a323453fb114b63fb3f8b1e3a6bca
MutasemShboul <mutasemshboul14@gmail.com>
2023-05-23 23:43:42 +0300

    Mutasem V2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public int addNumbers(int a, int b) {
-        int sum = a + b;
-        // return value
-        return sum;
-    }
-
-    // create a method
-    public static int square(int num) {
-
-        // return statement
-        return num * num;
-    }
-    public void square1(int a) {
-  int square = a * a;
-  System.out.println("Square is: " + square);
-}
-
-
-public void display1() {
-    System.out.println("Method without parameter");
-  }
     public static void main(String[] args) {
 
     }
