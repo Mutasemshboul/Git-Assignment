fa59c900859309cebf6804c485226a6af37b9dff
MutasemShboul <mutasemshboul14@gmail.com>
2023-05-23 23:30:11 +0300

    Mutasem V1.7

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Helllo from here Hellooppp
-        //Osama is king
-        //Helllo from here ksdhfkjashdfasdf
-        //sdkfjkoldsljfsdfsdf
+
     }
 }
